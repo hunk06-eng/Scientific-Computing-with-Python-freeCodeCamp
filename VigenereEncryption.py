7d7b885e1008190321fb280aaae7ca43dbe0906b
Ahmad Taha <215596811+hunk06-eng@users.noreply.github.com>
2025-06-20 04:07:42 +0300

    Update VigenereEncryption.py
    improved efficiency by getting rid of the alphabet dictionary and used ascii timetable instead, added a while loop to let the user run the script as many times as they wanted.

--- VigenereEncryption.py
+++ VigenereEncryption.py
@@ -1,33 +1,22 @@
 print("*\n*\n*")
-print("It is not recommended to use this encryption method for sensitive data, in the past, vigenere encryption was hard to break; but with computers it became easier to crack, this code is recommended for educational purposes only.\n")
+print("DO NOT USE this encryption method for ANY sensitive data.\n")
 print("*\n*\n*\n")
 
-text = input('Enter the text you want to encrypt/decrypt: ')
-key = input('Enter the key to use for encryption/decryption: ')
-mode = int(input('Enter 1 for encryption, -1 for decryption: '))
-
-if len(key) == 0:
-    key = input('Please enter a key for the script to proceed: ')
-
-alphabet_index = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18,
-            't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25, 'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10, 'L':11,
-            'M':12, 'N':13, 'O':14, 'P':15, 'Q':16, 'R':17, 'S':18,'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25}
-
 def vigenere(text, key, mode=1):
-    global alphabet_index
     final_message = ''
     key_loc = 0
     
     for char in text:
         # pass any symbols, numbers, punctuation marks without encrypting/decrypting them.
-        if char not in alphabet_index:
+        if char.isalpha() == False:
             final_message += char
+
         else:
-            # find index of char
-            char_index = alphabet_index[char]
+            # find index of char using ascii table
+            char_index = (ord(char)-65) if char.isupper() else (ord(char)-97)
             # key_loc keeps track of which letter (in the key) the iteration stopped at during encryption/decryption process.
-            # modulo used to circle through the start of the key once the key_loc variable exceeds the key's length.
-            key_index = alphabet_index[key[key_loc % len(key)]]
+            # modulo is used to circle through the key once the key_loc variable exceeds the key's length.
+            key_index = (ord(key[key_loc % len(key)])-65) if key[key_loc % len(key)].isupper() else (ord(key[key_loc % len(key)]) - 97)
             key_loc += 1
 
             # same as before, modulo ensures that the rotation circles back to the start of the alphabet once it exceeds it's length.
@@ -36,15 +25,25 @@ def vigenere(text, key, mode=1):
             final_message += get_char_by_index(rotation, char.isupper())
     return final_message
 
-def get_char_by_index(index, isUpper):
-    global alphabet_index
+def get_char_by_index(rotation, isUpper):
+    # using the ascii table, if the character in the text is uppercase, add the rotation amount to 'A' -- > 65 on ascii table, then convert integer to character.
+    # if the character in the text is lowercase, add the rotation mount to 'a' -- > 97 on ascii table, then convert integer to character using chr().
 
-    for char, indx in alphabet_index.items():
-        if indx == index:
-            if isUpper:
-                return char.upper()
-            else:
-                return char
+    return (chr(65+rotation) if isUpper else chr(97+rotation))
 
-print(vigenere(text, key, mode))
-input("\nPress enter to exit.")
+# while loop so the script runs as many times as needed.
+while True:
+    text = input('Enter the text you want to encrypt/decrypt: ')
+    key = input('Enter the key to use for encryption/decryption: ')
+    mode = int(input('Enter 1 for encryption, -1 for decryption: '))
+
+    if len(key) == 0:
+        key = input('Please enter a key for the script to proceed: ')
+
+    print(vigenere(text, key, mode))
+
+    print("\nEnter 0 to restart script, 1 to close it.")
+    if int(input()):
+        break
+    
+    print('\n')
